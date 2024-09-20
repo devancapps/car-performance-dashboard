ec0a2275ad0ca0e6f2fb945e4b0cba767838212d
Devan Capps <devancapps97@gmail.com>
2024-09-19 23:54:42 -0500

    Improved theme and added icons for alert system

--- src/components/AlertSystem.js
+++ src/components/AlertSystem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { FaOilCan, FaExclamationTriangle, FaCar } from 'react-icons/fa';
 import './AlertSystem.css';
+
+const alertIcons = {
+  'oil': <FaOilCan />,
+  'engine': <FaExclamationTriangle />,
+  'general': <FaCar />
+};
 
 function AlertSystem({ alerts }) {
   return (
@@ -11,7 +18,10 @@ function AlertSystem({ alerts }) {
         <ul>
           {alerts.map((alert, index) => (
             <li key={index} className={`alert alert-${alert.severity}`}>
-              {alert.message}
+              <span className="alert-icon">
+                {alertIcons[alert.type] || alertIcons['general']}
+              </span>
+              <span className="alert-message">{alert.message}</span>
             </li>
           ))}
         </ul>
